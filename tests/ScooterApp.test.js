3210a6551806f0031604d44e01c253a477deb647
ghezaii <ghezai@google.com>
2023-09-29 19:14:25 -0400

    Passes tests for registerUser, loginUser, and logoutUser  methods

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -5,15 +5,25 @@ const ScooterApp = require('../src/ScooterApp')
 const scooterApp = new ScooterApp();
 // ScooterApp tests here
 
-// register user
 describe("registerUser method tests", () => {
+  // register user
   test("Should return instance of User", () => {
     let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
     expect(response).toBeInstanceOf(User);
   });
+  // log in
+  test("Should locate and log in user", () => {
+    scooterApp.registerUser("Random Man", "test123", 21);
+    scooterApp.loginUser("Random Man", "test123")
+    expect(scooterApp.registeredUsers["Random Man"].loggedIn).toBe(true)
+  })
+  test("Should locate and log out user", () => {
+    scooterApp.registerUser("Random Person", "test123", 21);
+    scooterApp.loginUser("Random Person", "test123")
+    scooterApp.logoutUser("Random Person")
+    expect(scooterApp.registeredUsers["Random Person"].loggedIn).toBe(false)
+  })
 });
-
-// log in
 
 // log out
 
