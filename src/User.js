06cc5ad17ec7d0b8ad3bd90b8d1ddd4cf48948a7
ghezaii <ghezai@google.com>
2023-09-29 18:49:27 -0400

    User class created with login and logout methods

--- src/User.js
+++ src/User.js
@@ -1,5 +1,23 @@
 class User {
   // User code here
+  constructor(username, password, age, loggedIn = false) {
+    this.username = username;
+    this.password = password; 
+    this.age = age;
+    this.loggedIn = loggedIn;
+  }
+
+  login(password) {
+    if (password === this.password) {
+      return this.loggedIn = true;
+    } else {
+      throw new Error("incorrect password");
+    }
+  }
+
+  logout() {
+    return this.loggedIn = false;
+  }
 }
 
 module.exports = User
