1a1d464521bdd8e9b92656f66a55773c8019b013
ghezaii <ghezai@google.com>
2023-10-01 16:59:42 -0400

    Passes tests for rent and dock methods

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -2,24 +2,42 @@ const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 
 //typeof scooter === object
-describe('scooter object', () => {
-  test('Scooter class should create Scooter instance', () => {
+describe('Scooter object', () => {
+  test('create scooter - instance of Scooter class', () => {
     const scooter = new Scooter();
     expect(scooter).toBeInstanceOf(Scooter);
   });
 })
 
 //Method tests
-describe('scooter methods', () => {
+describe('Scooter methods', () => {
   // tests here!
+  
+  let user;
+  let scooter;
+ 
+  beforeEach(() => {
+    user = new User('senai', 'swe123', 23)
+    scooter = new Scooter('station1')
+  })
 
   //rent method
+  test('rent scooter to user - set station to null', () => {
+    scooter.rent(user)
+    expect(scooter.user).toBe(user)
+    expect(scooter.station).toBe(null)
+  })
   
-
   //dock method
+  test('dock scooter - set user to null', () => {
+    scooter.rent(user)
+    scooter.dock('station1')
+    expect(scooter.user).toBe(null)
+    expect(scooter.station).toBe('station1')
+  })
 
   //requestRepair method
 
   //charge method
 
-})
+});
