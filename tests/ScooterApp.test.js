8a556f53d930a9708778db2b9d3e2925a288a649
ghezaii <ghezai@google.com>
2023-10-01 17:02:07 -0400

    Passes tests for all methods

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -5,28 +5,55 @@ const ScooterApp = require('../src/ScooterApp')
 const scooterApp = new ScooterApp();
 // ScooterApp tests here
 
-describe("registerUser method tests", () => {
+describe('Register, login, logout user methods tests', () => {
+
   // register user
-  test("Should return instance of User", () => {
+  test("register new user - instance of User class", () => {
     let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
     expect(response).toBeInstanceOf(User);
   });
+
   // log in
-  test("Should locate and log in user", () => {
-    scooterApp.registerUser("Random Man", "test123", 21);
-    scooterApp.loginUser("Random Man", "test123")
-    expect(scooterApp.registeredUsers["Random Man"].loggedIn).toBe(true)
-  })
-  test("Should locate and log out user", () => {
-    scooterApp.registerUser("Random Person", "test123", 21);
-    scooterApp.loginUser("Random Person", "test123")
-    scooterApp.logoutUser("Random Person")
-    expect(scooterApp.registeredUsers["Random Person"].loggedIn).toBe(false)
-  })
+  test("locate and login user", () => {
+    scooterApp.registerUser('senai', 'swe123', 23);
+    scooterApp.loginUser('senai', 'swe123')
+    expect(scooterApp.registeredUsers['senai'].loggedIn).toBe(true)
+  });
+
+  // log out
+  test("locate and logout user", () => {
+    scooterApp.registerUser('sani', 'swe123', 21);
+    scooterApp.loginUser('sani', 'swe123')
+    scooterApp.logoutUser('sani')
+    expect(scooterApp.registeredUsers['sani'].loggedIn).toBe(false)
+  });
 });
 
-// log out
+describe('Create, dock, rent scooter methods tests', () => {
+  
+  let scooter;
 
-// rent scooter
+  beforeEach(() => {
+    scooter = scooterApp.createScooter('station1')
+  })
 
-// dock scooter
+  // create scooter
+  test("create new scooter - instance of Scooter class", () => {
+    expect(scooter).toBeInstanceOf(Scooter)
+  });
+
+  // dock scooter
+  test("dock scooter", () => {
+    let user = scooterApp.registerUser('abel', 'swe123', 19)
+    scooterApp.rentScooter(scooter, user)
+    expect(scooterApp.dockScooter(scooter, 'station1')).toBe('scooter is docked')
+  });
+
+  // rent scooter
+  test("rent scooter to user", () => {
+    let user = scooterApp.registerUser('mati', 'swe123', 22)
+    scooterApp.rentScooter(scooter, user)
+    expect(scooter.user).toBe(user)
+    expect(scooter.station).toBe(null)
+  });
+});
